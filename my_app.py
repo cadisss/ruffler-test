4a3fbc8b71882dbe243a647a0e1ac74a6b7fce05
Hilda003 <Hildatulwardah003@gmail.com>
2025-10-12 12:32:42 +0700

    add import pyqt5

--- my_app.py
+++ my_app.py
@@ -1,1 +1,9 @@
 # write the code for main app and first screen
+
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
