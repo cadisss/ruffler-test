4fd544955d045ed00b73f7e02d29ba8fce0d2f6d
cadisss <5025251242@student.its.ac.id>
2025-10-19 16:17:09 +0700

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,6 +1,10 @@
-from  PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import(QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton,
-                            QPushButton,QLabel, QListWidget, QLineEdit)
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # checking the types of input values
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
 
 
 from instr import *
